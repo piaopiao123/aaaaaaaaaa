b66549b43907f307303fe51a0a0b3cc72eaf930d
piaopiao123 <382044567@qq.com>
2018-07-06 11:46:10 +0800

    asd

---
+++ src/A.java
@@ -0,0 +1,32 @@
+public class A {
+    private int a;
+    private String b;
+    private boolean c;
+
+    public A() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public boolean isC() {
+        return c;
+    }
+
+    public void setC(boolean c) {
+        this.c = c;
+    }
+}
